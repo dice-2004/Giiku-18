4b3face6825e328c39b2706bee0484b23e5ad710
tkhs-0114 <tkhs.mail0114@gmail.com>
2024-12-06 23:51:19 +0900

    キューブの表示

--- oshikatsu/app/page.tsx
+++ oshikatsu/app/page.tsx
@@ -1,7 +1,23 @@
+import React from 'react';
+import ThreeCube from '../components/ThreeCube';
+
+interface Face {
+  jumpUrl: string;
+  texture: string;
+}
+
 export default function Home() {
+  const face: Face[] = [
+    { jumpUrl: "/hikakin", texture: "/hika.jpg" },
+    { jumpUrl: "/evimarabo", texture: "/evim.jpg" },
+    { jumpUrl: "/hito", texture: "/hito.jpg" },
+    { jumpUrl: "/kumagoro", texture: "/kuma.jpg" },
+    { jumpUrl: "/noneuser", texture: "https://yt3.googleusercontent.com/emNJUpccv67FT828Z1g0mfSB0hdLBPfIf_4IMxOfpcMCepnt7ABvk3bAZXBQZJ6fZwNcl0KCNQ=s160-c-k-c0x00ffffff-no-rj" },
+    { jumpUrl: "/reis", texture: "https://yt3.googleusercontent.com/eZxbjM77Nkzxg8MLTKr1gzreBkYZpZ2ITBiXx04W_6V7s0taEmMgGW1LQRU7kbaLHDmKSmk5ZQ=s160-c-k-c0x00ffffff-no-rj" },
+  ];
   return (
       <div>
-          <p>メインコンテンツ1</p>
+					<ThreeCube face={face} />
           <p>メインコンテンツ2</p>
           <p>メインコンテンツ3</p>
           <p>メインコンテンツ4</p>
